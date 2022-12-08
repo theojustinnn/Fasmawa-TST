2d9e4ad07a6ebac843b8f20f01f97b7550e4cd68
theojustinnn <114268813+theojustinnn@users.noreply.github.com>
2022-12-08 08:59:07 +0700

    Update main

--- main.py
+++ main.py
@@ -2,19 +2,28 @@ from fastapi import FastAPI
 
 import uvicorn
 
-# App routers
-# from auth import routers as AuthRouter
+# Routers yang digunakan
+from auth import routers as AuthRouter
 from user import routers as UserRouter
 from fasilitas import routers as FasilitasRouter
+from fasilitas import routers2 as KeramaianRouter
 from lokasi import routers as LokasiRouter
+from listfasilitas import routers as ListRouter
 
 
 app = FastAPI()
 
-# app.include_router(AuthRouter.router)
+# Include semua routers
+app.include_router(AuthRouter.router)
 app.include_router(UserRouter.router)
 app.include_router(FasilitasRouter.router)
+app.include_router(KeramaianRouter.router)
 app.include_router(LokasiRouter.router)
+app.include_router(ListRouter.router)
+
+@app.get("/")
+async def home():
+	return {"Selamat Datang di Fasmawa ITB" : "Silahkan login terlebih dahulu"}
 
 if __name__ == '__main__':
 	uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
