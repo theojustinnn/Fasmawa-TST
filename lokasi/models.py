b136523ef29c315afe9f0a4e6049cc278bd617b5
theojustinnn <114268813+theojustinnn@users.noreply.github.com>
2022-12-08 08:30:43 +0700

    Update lokasi models

--- lokasi/models.py
+++ lokasi/models.py
@@ -1,10 +1,7 @@
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
 
 from database import connection
-
-# Import relations
 from user.models import User
 
 class Lokasi(connection.Base):
@@ -13,9 +10,3 @@ class Lokasi(connection.Base):
     id = Column(Integer, primary_key=True, index=True)
     nama = Column(String, index=True)
     kapasitas = Column(Integer)
-
-	# Foreign key
-    # user_id = Column(Integer, ForeignKey('users.id'))
-
-	# Relationships
-    # writer = relationship('User', back_populates='notes')
