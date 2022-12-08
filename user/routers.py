fdbe1b62b893e6378678bf2dec16c34fb697b6b5
theojustinnn <114268813+theojustinnn@users.noreply.github.com>
2022-12-08 08:57:34 +0700

    Update user routers

--- user/routers.py
+++ user/routers.py
@@ -2,6 +2,7 @@ from typing import List
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database import connection
+from auth import schemas as AuthSchemas, services as AuthServices
 
 from . import models, schemas, services
 
@@ -10,15 +11,15 @@ router = APIRouter(
 	tags=['Users']
 )
 
-# Initialize table to database
+# Inisialisasi database user
 models.connection.Base.metadata.create_all(bind=connection.engine)
 
-# Create user
+# Create user baru
 @router.post('', status_code=201)
 async def create_user(user:schemas.CreateUser, db:Session=Depends(connection.get_db)):
 	return await services.create_user(user=user, db=db)
 
-# Get all users
+# Get semua user
 @router.get('', response_model=List[schemas.GetUser])
-async def get_all_users(db:Session=Depends(connection.get_db)):
+async def get_all_users(db:Session=Depends(connection.get_db), authenticated_user: AuthSchemas.AuthenticatedUser=Depends(AuthServices.get_current_user)):
 	return await services.get_all_users(db=db)
