482c163031679064d0fcda72ea98fb28d800c725
theojustinnn <114268813+theojustinnn@users.noreply.github.com>
2022-12-08 08:58:26 +0700

    Update user services

--- user/services.py
+++ user/services.py
@@ -1,12 +1,12 @@
 from typing import List
 from sqlalchemy.orm import Session
-# from auth.services import Hash
+from auth.services import Hash
 
 from . import models, schemas
 
 # Create user
 async def create_user(user:schemas.CreateUser, db:Session) -> schemas.GetUser:
-	# user.password = Hash.encrypt(user.password)
+	user.password = Hash.encrypt(user.password)
 	new_user = models.User(**user.dict())
 	db.add(new_user)
 	db.commit()
@@ -15,7 +15,7 @@ async def create_user(user:schemas.CreateUser, db:Session) -> schemas.GetUser:
 	db.refresh(new_user)
 	return schemas.GetUser.from_orm(new_user)
 
-# Get all user
-async def get_all_users(db:Session) -> List[schemas.GetUser]:
+# Get semua user
+async def get_semua_users(db:Session) -> List[schemas.GetUser]:
 	users = db.query(models.User).all()
 	return list(map(schemas.GetUser.from_orm, users))
