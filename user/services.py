27721290430af52157fc1f0ba512939f1ce1b2c0
theojustinnn <114268813+theojustinnn@users.noreply.github.com>
2022-12-09 17:07:56 +0700

    Update services.py

--- user/services.py
+++ user/services.py
@@ -4,18 +4,17 @@ from auth.services import Hash
 
 from . import models, schemas
 
-# Create user
+# Create user baru
 async def create_user(user:schemas.CreateUser, db:Session) -> schemas.GetUser:
 	user.password = Hash.encrypt(user.password)
 	new_user = models.User(**user.dict())
 	db.add(new_user)
 	db.commit()
 
-	# Return the newly created user
 	db.refresh(new_user)
 	return schemas.GetUser.from_orm(new_user)
 
 # Get semua user
-async def get_semua_users(db:Session) -> List[schemas.GetUser]:
+async def get_all_users(db:Session) -> List[schemas.GetUser]:
 	users = db.query(models.User).all()
 	return list(map(schemas.GetUser.from_orm, users))
